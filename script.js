0204935660affc8d8b6d89d0068fbaa5f2e5edd1
kevinmarincardenas <kevinmarin1012@gmail.com>
2024-11-27 07:09:53 -0500

    text

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let slideIndex = 0;
 function showSlides() {
-    const slides = document.querySelectorAll('.carousel-slide img');
+    const slides = document.querySelectorAll('.carousel-slide a');
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
-    } slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = 'block';
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = 'block';
     setTimeout(showSlides, 3000); // Cambia la imagen cada 3 segundos
 }
 function moveSlide(n) {
