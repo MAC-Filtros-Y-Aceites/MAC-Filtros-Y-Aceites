b1126f62e613efac123e7ef65af7cfa4a1b9198e
Ye-Florez <yelitzsarayfo@ufps.edu.co>
2024-11-27 08:33:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let slideIndex = 0;
 function showSlides() {
-    const slides = document.querySelectorAll('.carousel-slide a');
+    const slides = document.querySelectorAll('.carousel-slide img');
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = 'block';
+    } slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = 'block';
     setTimeout(showSlides, 3000); // Cambia la imagen cada 3 segundos
 }
 function moveSlide(n) {
